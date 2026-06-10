4960ba4616ffc9ed488ad8fe98f46d3af892749e
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-10 11:56:59 -0500

    commit - Resultado .

--- src/modelo/Resultado.java
+++ src/modelo/Resultado.java
@@ -1,4 +1,64 @@
 package modelo;
 
 public class Resultado {
+
+    private double a;
+    private double b;
+    private double c;
+    private double d;
+
+    private double raizReal;
+
+    public Resultado(double a,
+                     double b,
+                     double c,
+                     double d,
+                     double raizReal) {
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.raizReal = raizReal;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    public double getRaizReal() {
+        return raizReal;
+    }
+
+    public void setRaizReal(double raizReal) {
+        this.raizReal = raizReal;
+    }
 }
